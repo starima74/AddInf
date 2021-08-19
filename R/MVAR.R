f88d4dfdc44c1d0868eabb08fb26f9f908b7b516
Sergey Tarima <sergey.s.tarima@gmail.com>
2021-08-19 14:01:51 -0500

    Update on Aug 19

--- R/MVAR.R
+++ R/MVAR.R
@@ -21,13 +21,13 @@ function(dd, theta.f, Add.Inf,
       boot.hat <- c(boot.hat,Add.Inf$Functions[[j]](dd[ind,]))
     bootres[i,] <- boot.hat
   }
-  K = var(bootres)/nrow(dd)
+  K = var(bootres)
   ind1 <- 1:thetahat.length
   ind2 <- (thetahat.length+1):(betahat.length+thetahat.length)
-  K11 <- K[ind1,ind1] * n
-  K12 <- K[ind1,ind2] * n
-  K21 <- K[ind2,ind1] * n
-  K22 <- K[ind2,ind2] * n
+  K11 <- K[ind1,ind1]
+  K12 <- K[ind1,ind2]
+  K21 <- K[ind2,ind1]
+  K22 <- K[ind2,ind2]
   K.add <- as.matrix(Matrix::bdiag(Add.Inf$Vars))
   betatilde <- unlist(Add.Inf$Means)
   SVD_var <- svd(K22 + K.add)
@@ -42,7 +42,7 @@ function(dd, theta.f, Add.Inf,
     Kinv <- SVD_var$v %*% diag_elem %*% t(SVD_var$u)
   if(length(SVD_var$d>0 & ind.svd == 1) > 1)  
     Kinv <- SVD_var$v %*% diag(diag_elem) %*% t(SVD_var$u)
-  thetahat.MVAR = thetahat + K12 %*% Kinv %*% (betahat - betatilde)
+  thetahat.MVAR = thetahat - K12 %*% Kinv %*% (betahat - betatilde)
   thetahat.MVAR.Var = K11 - K12 %*% Kinv %*% K21
   list(Theta.Est = thetahat.MVAR, 
        Theta.Est.Var = thetahat.MVAR.Var,
